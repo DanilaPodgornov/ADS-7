ac78e60be4bb62aa0f133e026e48fe990fd3a33a
DanilaPodgornov <116427593+DanilaPodgornov@users.noreply.github.com>
2023-04-30 21:57:28 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -5,6 +5,80 @@
 template<typename T>
 class TPQueue {
   // реализация шаблона очереди с приоритетом на связанном списке
+private:
+    struct Node {
+        T value;
+        Node* next;
+        Node* prev;
+    };
+    Node* head;
+    Node* tail;
+    Node* create(const T& value) {
+        Node* node = new Node;
+        node->value = value;
+        node->next = nullptr;
+        node->prev = nullptr;
+        return node;
+    }
+
+public:
+    TPQueue():head(nullptr), tail(nullptr) {}
+
+    void push(const T& value) {
+        /*if (head && tail) {
+            tail->next = create(value);
+            tail = tail->next;
+        } else {
+            head = tail = create(value);
+        }*/
+
+        if (head == nullptr) {
+            head = tail = create(value);
+        } else if (value.prior <= tail->value.prior) {
+            tail->next = create(value);
+            tail->next->prev = tail;
+            tail = tail->next;
+        } else if (head == tail) {
+            tail->prev = create(value);
+            head = tail->prev;
+            head->next = tail;
+        } else if (tail->value.prior < value.prior) {
+            Node* temp = tail;
+            while (temp != head && value.prior > temp->value.prior) {
+                temp = temp->prev;
+            }
+            if (temp->value.prior >= value.prior) {
+                Node* node = create(value);
+                temp->next->prev = node;
+                node->next = temp->next;
+                temp->next = node;
+            } else if (temp == head && temp->value.prior < value.prior) {
+                head->prev = create(value);
+                head = head->prev;
+                head->next = temp;
+            }
+        }
+    }
+
+    const T& pop() {
+        if (!head) {
+            throw std::string("Queue is empty.");
+        } else {
+            Node* temp = head->next;
+            T value = head->value;
+            delete head;
+            head = temp;
+            return value;
+        }
+    }
+
+    Node* search(const T& value) {
+        Node* temp = tail;
+        while (temp != nullptr && value.prior > temp->value.prior) {
+            temp = temp->prev;
+        }
+        return temp;
+    }
 };
 
 struct SYM {
